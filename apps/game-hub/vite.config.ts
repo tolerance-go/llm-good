6e881190e117b1faa5037ec544322c79fad69b65
yarnb <yarnb@qq.com>
2024-12-25 16:11:03 +0800

    chore

--- apps/game-hub/vite.config.ts
+++ apps/game-hub/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/game-hub/' : '/',
   plugins: [react()],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
-  },
-})
+}))
